75678f296b7a3a5dacfbec176e26186ba3110d17
Charlie Lynch-Gerrard <96972078+charleslgdesign@users.noreply.github.com>
2024-05-22 18:50:47 +1200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -253,9 +253,9 @@
 
     function preload() {
     soundFormats('mp3');
-    // setBuffer(1);
+    setBuffer(1);
     kick = loadSound('audio/kick');
-    // kick.setBuffer(1);
+    // .setBuffer(1);
     glaring_violin = loadSound('audio/glaring_violin');
     // glaring_violin.setBuffer(1);
     liquid_synth = loadSound('audio/liquid_synth');
